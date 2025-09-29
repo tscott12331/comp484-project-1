8787ec316a65d1b5bb2e2499e1e1951d2a9aada9
Thomas Scott <t.scott12331@gmail.com>
2025-09-29 14:25:10 -0700

    done

--- email.js
+++ email.js
@@ -1,10 +1,19 @@
+// fetches the form element using a document function to
+// get the element by its id
 const formEl = document.getElementById('email-form');
 
 formEl.addEventListener('submit', (e) => {
+    // prevents default behavior of submitting a form which
+    // would refresh the page
     e.preventDefault();
 
+    // initializes a data structure for the data in the form element
     const data = new FormData(formEl);
+    // gets the value of 'message' in the form
+    // 'message' comes from the name i gave to the textarea in the name attribute
     const message = data.get('message');
     
+    // opens the users email client with a default subject and their message
+    // the recipient is my email
     window.open(`mailto:t.scott12331@gmail.com?subject=${encodeURIComponent('My favorite trips')}&body=${encodeURIComponent(message)}`);
 })
